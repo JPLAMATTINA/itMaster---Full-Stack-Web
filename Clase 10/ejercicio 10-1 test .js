58525fb4fd8230bc804e3297f159b6798ac99d31
Juan Pablo La Mattina <69357251+JPLAMATTINA@users.noreply.github.com>
2020-09-11 19:11:40 -0300

    commit "Web Full Stack"

---
+++ Clase 10/ejercicio 10-1 test .js
@@ -0,0 +1,12 @@
+
+var n1=10;
+var n2=4;
+var n3=7;
+var n4=8;
+var alumno="juan"
+resultado=((n1+n2+n3+n4)/4);
+
+    if (resultado<7)
+        console.log("aprobado " + alumno)
+        else 
+            console.log ("reprobado " + alumno)
