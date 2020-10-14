8eca07c7b8cc5af3d35c4006758a94c280031c71
Juan Pablo La Mattina <69357251+JPLAMATTINA@users.noreply.github.com>
2020-10-14 15:46:29 -0300

    prueba

---
+++ React/Clase 20/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ContainedButtons from './menu.js'
+
+const App = () => (
+  <>
+   <ContainedButtons/>
+  </>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
